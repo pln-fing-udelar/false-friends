4d9fca71ff276e2505fb2210e3ee8f05a9c92e6f
Jairo Bonanata <jbonanat@gmail.com>
2016-02-03 19:14:53 -0300

    agregué palabras similares y comentario de los false friends usando wordnet

--- similar_words.py
+++ similar_words.py
@@ -3,8 +3,9 @@
 
 import icu
 from nltk.corpus import wordnet as wn
+import unicodedata
 
-if __name__ == '__main__':
+def exact_matching():
     spa_words = set(wn.all_lemma_names(lang='spa'))
     por_words = set(wn.all_lemma_names(lang='por'))
 
@@ -13,4 +14,25 @@ if __name__ == '__main__':
     collator = icu.Collator.createInstance(icu.Locale('es_ES.UTF-8'))
     common_words_sorted = sorted(common_words, key=collator.getSortKey)
 
-    print('\n'.join(common_words_sorted))
+    return common_words
+    # print('\n'.join(common_words_sorted))
+
+
+#
+def similar_matching():
+    # por_words = set(wn.all_lemma_names(lang='por'))
+    def change_similar_letters(word):
+        return unicodedata.normalize('NFKD', word).encode("ascii", "ignore").decode("ascii")
+    
+    spa_words = set([change_similar_letters(word) for word in wn.all_lemma_names(lang='spa')])
+    por_words = set([change_similar_letters(word) for word in wn.all_lemma_names(lang='por')])
+
+    common_words = spa_words & por_words
+
+    collator = icu.Collator.createInstance(icu.Locale('es_ES.UTF-8'))
+    common_words_sorted = sorted(common_words, key=collator.getSortKey)
+    return common_words
+    # print('\n'.join(common_words_sorted))
+
+# baseline - check if
+# ff = [x for x in set(wn.all_lemma_names(lang="spa")) & set(wn.all_lemma_names(lang="por")) if set(wn.synsets(x, lang="spa")) & set(wn.synsets(x, lang="por")) == set() ]
