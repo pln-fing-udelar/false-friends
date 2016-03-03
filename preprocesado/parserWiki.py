56f0d2d39bbe9b9671af77d22150a3897b531161
Jairo Bonanata <jbonanat@gmail.com>
2016-03-03 20:49:18 -0300

    Agregue mas cosas, queda faltando lo del portugues

--- preprocesado/parserWiki.py
+++ preprocesado/parserWiki.py
@@ -7,6 +7,7 @@ import re
 from lxml import etree
 from WikiExtractor import clean, dropNested, section
 from nltk.corpus import stopwords
+from nltk.tokenize import SpaceTokenizer
 import datetime
 
 
@@ -29,6 +30,19 @@ def parrafos(texto_wiki):
     return resultado
 
 
+def replace_numbers_with_words(s):
+    return s.replace('0', ' cero ')\
+        .replace('1', ' uno ')\
+        .replace('2', ' dos ')\
+        .replace('3', ' tres ')\
+        .replace('4', ' cuatro ')\
+        .replace('5', ' cinco ')\
+        .replace('6', ' seis' )\
+        .replace('7', ' siete ')\
+        .replace('8', ' ocho ')\
+        .replace('9', ' nueve ')\
+
+
 def preprocesar_wiki(infile):
     identificador = 0
     infile_xml = infile + ".xml"
@@ -39,6 +53,11 @@ def preprocesar_wiki(infile):
     #                          re.IGNORECASE | re.UNICODE | re.DOTALL)
     links_archivos = re.compile("\[\[([\s\(\)\w\-]*)\:([\s\(\)\w\-]*)[\s]*?\|*?[\s\(\)\w\-]*?\]\]".decode('utf-8'),
                                 re.IGNORECASE | re.UNICODE | re.DOTALL)
+    remove_punctuation_re = re.compile("\W", re.IGNORECASE | re.UNICODE)
+
+    def remove_non_letter(item):
+        return ' '.join(remove_punctuation_re.sub(' ', replace_numbers_with_words(item)).split())
+
     f = open(infile + '_preprocesada.txt', 'w')
     # por cada elemento del artículo de wikipedia
     for event, elem in context:
@@ -73,7 +92,7 @@ def preprocesar_wiki(infile):
             texto_text = clean(texto_text)
             texto_text = section.sub('', texto_text)
             texto_text = texto_text.replace('*', '')
-            texto_text = '\n'.join(filter(lambda item: item != '', map(lambda item: item.strip(), texto_text.split('\n'))))
+            texto_text = '\n'.join(filter(lambda item: item != '', map(remove_non_letter, texto_text.split('\n'))))
             f.write(texto_text.encode('utf-8'))
             ### hasta aca venia el if
             titulo.clear()
