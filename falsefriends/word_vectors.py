28a5718413083cb052e87874bff1044c642b605c
Jairo Bonanata <jbonanat@gmail.com>
2016-03-08 22:50:35 -0300

    Ahora deberia andar bien basico, igual me quedo la duda de si no instalamos cualquier cosa...

--- falsefriends/word_vectors.py
+++ falsefriends/word_vectors.py
@@ -1,3 +1,14 @@
 # -*- coding: utf-8 -*-
 
 
+import word2vec
+
+
+def train_model(infile, outfile):
+    word2vec.word2vec(infile, outfile, size=100, verbose=True)
+    model = word2vec.load(outfile)
+    model.vocab
+
+
+
+
