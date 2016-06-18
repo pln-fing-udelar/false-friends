43ce2238682baee50fe1b4eb2caff84d4dc76691
Santiago Castro <bryant@montevideo.com.uy>
2016-06-18 18:53:32 -0300

    Edit to accept classification input as training and testing separate files (#22)
    :+1:

--- falsefriends/classifier.py
+++ falsefriends/classifier.py
@@ -34,26 +34,7 @@ def calculate_measures(tn, fp, fn, tp):
 
 
 # noinspection PyPep8Naming
-def classify_friends_and_predict(friend_pairs, model_es, model_pt, translation_matrix, clf=svm.SVC(), n_folds=5):
-    friend_pairs = [friend_pair for friend_pair in friend_pairs
-                    if friend_pair.word_es in model_es.vocab and friend_pair.word_pt in model_pt.vocab]
-
-    vector_pairs = [[model_es[friend_pair.word_es], model_pt[friend_pair.word_pt]]
-                    for friend_pair in friend_pairs]
-
-    vectors_es, vectors_pt = zip(*vector_pairs)
-
-    translations_es = np.dot(vectors_es, translation_matrix)
-
-    distances = [spatial.distance.cosine(translation_es, vector_pt)
-                 for (translation_es, vector_pt) in zip(translations_es, vectors_pt)]
-
-    X = np.array([[distance] for distance in distances])
-    y = np.array([friend_pair.true_friends for friend_pair in friend_pairs])
-
-    logging.info("scaling features")
-    X = preprocessing.StandardScaler().fit_transform(X)
-
+def classify_with_cross_validation(X, y, clf=svm.SVC(), n_folds=5):
     cv_matrices = []
     cv_measures = defaultdict(list)  # FIXME: use collections.OrderedDict too
 
@@ -78,18 +59,38 @@ def classify_friends_and_predict(friend_pairs, model_es, model_pt, translation_m
         delta = np.std(measure_values) * 1.96 / math.sqrt(n_folds)  # 95% of confidence
         cv_measures[measure_name] = (mean, delta)
 
-    # X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
-    #
-    # logging.info("classifying")
-    # clf.fit(X_train, y_train)
-    #
-    # y_predicted = clf.predict(X_test)
-    #
-    # tn = sum(1 for test, predicted in zip(y_test, y_predicted) if not test and not predicted)
-    # fp = sum(1 for test, predicted in zip(y_test, y_predicted) if not test and predicted)
-    # fn = sum(1 for test, predicted in zip(y_test, y_predicted) if test and not predicted)
-    # tp = sum(1 for test, predicted in zip(y_test, y_predicted) if test and predicted)
-    #
-    # single_run_measure = calculate_measures(tn, fp, fn, tp)
-
     return cv_measures
+
+
+# noinspection PyPep8Naming
+def classify(X_train, X_test, y_train, y_test, clf=svm.SVC()):
+    logging.info("classifying")
+    clf.fit(X_train, y_train)
+
+    y_predicted = clf.predict(X_test)
+
+    tn = sum(1 for test, predicted in zip(y_test, y_predicted) if not test and not predicted)
+    fp = sum(1 for test, predicted in zip(y_test, y_predicted) if not test and predicted)
+    fn = sum(1 for test, predicted in zip(y_test, y_predicted) if test and not predicted)
+    tp = sum(1 for test, predicted in zip(y_test, y_predicted) if test and predicted)
+
+    return calculate_measures(tn, fp, fn, tp)
+
+
+# noinspection PyPep8Naming
+def features_labels_and_scaler(friend_pairs, model_es, model_pt, translation_matrix, scaler=None):
+    friend_pairs = [friend_pair for friend_pair in friend_pairs
+                    if friend_pair.word_es in model_es.vocab and friend_pair.word_pt in model_pt.vocab]
+    vector_pairs = [[model_es[friend_pair.word_es], model_pt[friend_pair.word_pt]]
+                    for friend_pair in friend_pairs]
+    vectors_es, vectors_pt = zip(*vector_pairs)
+    translations_es = np.dot(vectors_es, translation_matrix)
+    distances = [spatial.distance.cosine(translation_es, vector_pt)
+                 for (translation_es, vector_pt) in zip(translations_es, vectors_pt)]
+    X = np.array([[distance] for distance in distances])
+    y = np.array([friend_pair.true_friends for friend_pair in friend_pairs])
+    if scaler is None:
+        logging.info("scaling features")
+        scaler = preprocessing.StandardScaler().fit(X)
+    X = scaler.transform(X)
+    return X, y, scaler
