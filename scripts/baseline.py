098fb397db912c9fe798d2aeffa49110309120cc
Santiago Castro <santi.1410@hotmail.com>
2017-04-18 15:50:09 -0300

    Change baseline to only consider covered words and also to report coverage

--- scripts/baseline.py
+++ scripts/baseline.py
@@ -2,15 +2,23 @@
 # -*- coding: utf-8 -*-
 from nltk.corpus import wordnet as wn
 import numpy as np
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 from falsefriends import util
 
-testing_friend_pairs = util.read_words('resources/sepulveda2011_testing.txt')
+testing_friend_pairs = util.read_words('../resources/sepulveda2011_testing.txt')
 
-expected = np.asarray([friend_pair.true_friends for friend_pair in testing_friend_pairs])
+covered_pairs = [friend_pair for friend_pair in testing_friend_pairs
+                 if friend_pair.word_es in wn.all_lemma_names(lang='spa')
+                 and friend_pair.word_pt in wn.all_lemma_names(lang='por')]
+
+expected = np.asarray([friend_pair.true_friends for friend_pair in covered_pairs])
 
 outcome = np.asarray([any(friend_pair.word_pt in synset.lemma_names(lang='por')
                           for synset in wn.synsets(friend_pair.word_es, lang='spa'))
-                      for friend_pair in testing_friend_pairs])
+                      for friend_pair in covered_pairs])
 
 print('Accuracy: {}'.format(np.mean(expected == outcome)))
+print('Coverage: {}'.format(len(covered_pairs) / len(testing_friend_pairs)))
