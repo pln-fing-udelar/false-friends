f6b4ad91486b3065e2ee1fc1a11a44c5a7f57660
Santiago Castro <santi.1410@hotmail.com>
2017-04-20 18:25:25 -0300

    Change to test the baseline with the whole dataset

--- scripts/baseline.py
+++ scripts/baseline.py
@@ -10,9 +10,11 @@ sys.path.insert(0, PARENT_DIR)
 
 from falsefriends import util
 
-testing_friend_pairs = util.read_words(PARENT_DIR + '/resources/sepulveda2011_testing.txt')
+# As we are comparing methods with cross-validation and this method doesn't fit parameters,
+# we evaluate with the whole dataset.
+friend_pairs = util.read_words(PARENT_DIR + '/resources/sepulveda2011_original.txt')
 
-covered_pairs = [friend_pair for friend_pair in testing_friend_pairs
+covered_pairs = [friend_pair for friend_pair in friend_pairs
                  if friend_pair.word_es in wn.all_lemma_names(lang='spa')
                  and friend_pair.word_pt in wn.all_lemma_names(lang='por')]
 
@@ -23,4 +25,4 @@ outcome = np.asarray([any(friend_pair.word_pt in synset.lemma_names(lang='por')
                       for friend_pair in covered_pairs])
 
 print('Accuracy: {}'.format(np.mean(expected == outcome)))
-print('Coverage: {}'.format(len(covered_pairs) / len(testing_friend_pairs)))
+print('Coverage: {}'.format(len(covered_pairs) / len(friend_pairs)))
