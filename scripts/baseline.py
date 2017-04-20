3a95f3cea0433a08b57ac245414a7580c975d3a1
Jairo Bonanata <jbonanat@gmail.com>
2017-04-20 02:26:46 -0300

    Agregada coverage a la linea base

--- scripts/baseline.py
+++ scripts/baseline.py
@@ -5,12 +5,16 @@ import numpy as np
 
 from falsefriends import util
 
-testing_friend_pairs = util.read_words('resources/sepulveda2011_testing.txt')
+testing_friend_pairs = util.read_words('../resources/sepulveda2011_testing.txt')
 
-expected = np.asarray([friend_pair.true_friends for friend_pair in testing_friend_pairs])
+words_covered = list(filter(lambda friend_pair: len(wn.synsets(friend_pair.word_es, lang='spa')) > 0
+                                           and len(wn.synsets(friend_pair.word_pt, lang='por')) > 0
+                            , testing_friend_pairs))
+expected = np.asarray([friend_pair.true_friends for friend_pair in words_covered])
 
 outcome = np.asarray([any(friend_pair.word_pt in synset.lemma_names(lang='por')
                           for synset in wn.synsets(friend_pair.word_es, lang='spa'))
-                      for friend_pair in testing_friend_pairs])
+                      for friend_pair in words_covered])
 
 print('Accuracy: {}'.format(np.mean(expected == outcome)))
+print('Coverage: {}'.format(len(words_covered)/float(len(testing_friend_pairs))))
